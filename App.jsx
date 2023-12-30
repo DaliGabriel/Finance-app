74bc5ddf3e46811041b973bfbe7a5cbbf3472251
Gabriel Dalí <94218227+DaliGabriel@users.noreply.github.com>
2023-12-29 20:38:24 -0600

    UI del formulario para añadir registros manejando useState para trabajar con los datos del formulario

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 // You can import Ionicons from @expo/vector-icons if you use Expo or
 // react-native-vector-icons/Ionicons otherwise.
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, Image, TextInput, Button, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
@@ -9,11 +9,15 @@ import { Card, Icon } from '@rneui/themed';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useCallback } from 'react';
+import { useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
 
+//*Componente de la pantalla principal
 function HomeScreen() {
 
   return (
     <View style={styles.textContent}>
+      {/* Bienvenido */}
       <Text
         style={{
           fontFamily: "Poppins-Bold",
@@ -24,9 +28,10 @@ function HomeScreen() {
       >
         !Bienvenido! 👋
       </Text>
+      {/* Balance total */}
       <Text
         style={{
-          paddingTop: 20,
+          paddingTop: 10,
           paddingLeft: 20,
           fontSize: 40,
           fontFamily: "Poppins-Bold",
@@ -34,7 +39,6 @@ function HomeScreen() {
       >
         Balance total
       </Text>
-
       {/* Card de balance total */}
       <Card
         //*Estilos del card
@@ -53,15 +57,15 @@ function HomeScreen() {
           $66699
         </Text>
       </Card>
-
+      {/* Cards de ingresos y egresos */}
       <View style={styles.twoColumns}>
-        {/* Card de ingresos */}
+        {/* Card de total de ingresos */}
         <Card
           containerStyle={{
             borderColor: "green",
             borderWidth: 2,
             borderRadius: 8,
-            width: '30%'
+            width: "30%",
           }}
         >
           <Icon name="arrow-upward" color="green" />
@@ -87,14 +91,13 @@ function HomeScreen() {
             $666
           </Text>
         </Card>
-
-        {/* Card de egresos */}
+        {/* Card de total de egresos */}
         <Card
           containerStyle={{
             borderColor: "red",
             borderWidth: 2,
             borderRadius: 8,
-            width: '30%'
+            width: "30%",
           }}
         >
           <Icon name="arrow-downward" color="red" />
@@ -122,14 +125,160 @@ function HomeScreen() {
           </Text>
         </Card>
       </View>
+
+      {/* Registros*/}
+      {/* Ultimos registros*/}
+      <Text
+        style={{
+          paddingTop: 20,
+          paddingLeft: 20,
+          fontSize: 30,
+          fontFamily: "Poppins-Bold",
+        }}
+      >
+        Últimos registros
+      </Text>
+
+      {/* lista de registros */}
+      <ScrollView
+        style={{
+          width: "100%",
+        }}
+      >
+
+        {/*Carta de egresos*/}
+        <Card>
+          <View style={styles.cardContent}>
+            <Icon name="cash-plus" type="material-community" size={40}  color={'red'}/>
+            <View style={styles.textContainer}>
+              <Text style={styles.title}>Alimentos y bebidas</Text>
+              <Text>Comida semana</Text>
+            </View>
+            <View style={styles.amountContainer}>
+              <Text style={styles.amountIncome}>-$1,000</Text>
+            </View>
+          </View>
+        </Card>
+
+        {/*Carta de ingresos*/}
+        <Card>
+          <View style={styles.cardContent}>
+            <Icon name="cash-minus" type="material-community" size={40} color={'green'} />
+            <View style={styles.textContainer}>
+              <Text style={styles.title}>Salario</Text>
+              <Text>Quincena</Text>
+            </View>
+            <View style={styles.amountContainer}>
+              <Text style={styles.amountExpense}>+$7,000</Text>
+            </View>
+          </View>
+        </Card>
+
+        <Card>
+          <View style={styles.cardContent}>
+            <Icon name="cash-plus" type="material-community" size={40}  color={'red'}/>
+            <View style={styles.textContainer}>
+              <Text style={styles.title}>Alimentos y bebidas</Text>
+              <Text>Comida semana</Text>
+            </View>
+            <View style={styles.amountContainer}>
+              <Text style={styles.amountIncome}>-$1,000</Text>
+            </View>
+          </View>
+        </Card>
+
+      </ScrollView>
     </View>
   );
 }
 
+//*Componente del formulario para añadir gastos e ingresos
 function AddScreen() {
+
+  const [formValues, setFormValues] = useState({
+    cantidad: '',
+    categoria: '',
+    comentario: '',
+  });
+
+  const categorias = [
+    'Alimentos y bebidas',
+    'Salud e higiene',
+    'Transporte',
+    'Diversión',
+    'Hogar',
+    'Pago de servicios',
+    'Otros gastos'
+  ];
+
+  const handleInputChange = (name, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
   return (
-    <View style={styles.textCenter}>
-      <Text>Add</Text>
+    <View style={styles.containerForm}>
+      <Text
+        style={{
+          fontFamily: "Poppins-Medium",
+          fontSize: 20,
+          paddingTop: 30,
+        }}
+      >
+        Cantidad
+      </Text>
+      <TextInput
+        style={styles.inputAmount}
+        placeholder="$0.00"
+        value={formValues.cantidad}
+        onChangeText={(value) => handleInputChange("cantidad", value)}
+      />
+      <Text
+        style={{
+          fontFamily: "Poppins-Medium",
+          fontSize: 20,
+          paddingTop: 30,
+        }}
+      >
+        Selecciona una categoria
+      </Text>
+      <View style={styles.pickerContainer}>
+        <Picker
+          selectedValue={formValues.categoria}
+          onValueChange={(value) => handleInputChange("categoria", value)}
+        >
+          {categorias.map((categoria) => (
+            <Picker.Item key={categoria} label={categoria} value={categoria} />
+          ))}
+        </Picker>
+      </View>
+
+      <Text
+        style={{
+          fontFamily: "Poppins-Medium",
+          fontSize: 20,
+          paddingTop: 30,
+        }}
+      >
+        Agregar un comentario
+      </Text>
+      <TextInput
+        style={styles.inputComment}
+        placeholder="Comentario"
+        value={formValues.comentario}
+        onChangeText={(value) => handleInputChange("comentario", value)}
+      />
+
+      <TouchableOpacity
+        style={styles.buttonForm}
+        onPress={() => {
+          console.log(formValues)
+        }}
+      >
+        <Text style={styles.buttonText}>Agregar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -188,8 +337,74 @@ const styles = {
     justifyContent: "center",
     alignItems: "center",
   },
-  twoColumns:{ 
-    flexDirection: "row", 
-    justifyContent: "space-between" 
+  twoColumns: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  //*Estilos de la lista de registros
+  cardContent: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  textContainer: {
+    flex: 1,
+    marginLeft: 20,
+  },
+  amountContainer: {
+    alignItems: "flex-end",
+  },
+  //*titulo de lista de registro
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  //*monto del registro
+  amountIncome: {
+    fontSize: 25,
+    color: "red",
+  },
+  amountExpense: {
+    fontSize: 25,
+    color: "green",
+  },
+
+  //*Estilos del formulario para añadir registros
+  containerForm: {
+    padding: 20,
+  },
+  inputAmount: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    borderRadius: 5,
+  },
+  pickerContainer: {
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  inputComment: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    borderRadius: 5,
+  },
+  buttonForm: {
+    backgroundColor: "black",
+    padding: 10,
+    borderRadius: 20,
+    marginTop: 50,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    paddingTop:5,
+    paddingBottom:5,
   },
 };
